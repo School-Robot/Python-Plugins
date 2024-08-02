6800aa6c090b08c948f27c6568ecb9cb6c6d730f
Administrator <admin@mcsog.tk>
2024-08-02 18:11:59 +0800

    update

--- tk_mcsog_easy_reply.py
+++ tk_mcsog_easy_reply.py
@@ -1,9 +1,9 @@
 import json
-import threading
+import os
 
 class Plugin(object):
     plugin_methods={'register':{'priority':30000,'func':'register','desc':'注册插件'},'enable':{'priority':30000,'func':'enable','desc':'启用插件'},'disable':{'priority':30000,'func':'disable','desc':'禁用插件'},'unregister':{'priority':30000,'func':'unregister','desc':'卸载插件'},'group_message':{'priority':30000,'func':'group_message','desc':'群消息处理'}}
-    plugin_commands={}
+    plugin_commands={'easy_reply':'easy_reply_command','help':'easy_reply set <管理员QQ号> - 设置管理员'}
     plugin_auths={'send_group_msg'}
     auth=''
     log=None
@@ -12,7 +12,7 @@ class Plugin(object):
     util=None
     dir=None
     stop=False
-    config=None
+    config={'manager':0,'cmd':{'add':'+xx','del':'-xx'}}
     engine=None
     session=None
     dict_list={}
@@ -27,11 +27,19 @@ class Plugin(object):
     def enable(self,auth):
         self.auth=auth
         try:
-            with open(self.dir+'/dicts.json','r') as f:
+            with open(os.path.join(self.dir, 'dicts.json'),'r') as f:
                 self.dict_list=json.loads(f.read())
         except:
-            with open(self.dir+'/dicts.json','w') as f:
+            with open(os.path.join(self.dir, 'dicts.json'),'w') as f:
                 f.write(json.dumps({'test':'test'}))
+        try:
+            with open(os.path.join(self.dir,'config.json'),'r') as f:
+                self.config=json.loads(f.read())
+        except:
+            with open(os.path.join(self.dir,'config.json'),'w') as f:
+                f.write(json.dumps(self.config))
+        if self.config['manager']==0:
+            self.log.warning("未设置管理员，在控制台使用easy_reply set <管理员QQ号> 命令设置管理员")
         self.log.info("Plugin enable")
 
     def disable(self):
@@ -46,8 +54,8 @@ class Plugin(object):
         self.log.info("Plugin unregister")
 
     def group_message(self,time,self_id,sub_type,message_id,group_id,user_id,anonymous,message,raw_message,font,sender):
-        if raw_message.startswith('+xx'):
-            if user_id == 1822245039:
+        if raw_message.startswith(self.config['cmd']['add']):
+            if user_id == self.config['manager']:
                 msgs=raw_message.split(' ')
                 if len(msgs) == 3:
                     self.dict_list[msgs[1]]=msgs[2]
@@ -59,8 +67,8 @@ class Plugin(object):
             else:
                 self.util.send_group_msg(self.auth,group_id,'无权限')
                 return True
-        if raw_message.startswith('-xx'):
-            if user_id == 1822245039:
+        if raw_message.startswith(self.config['cmd']['del']):
+            if user_id == self.config['manager']:
                 msgs=raw_message.split(' ')
                 if len(msgs) == 2:
                     try:
@@ -79,6 +87,21 @@ class Plugin(object):
             self.util.send_group_msg(self.auth,group_id,self.dict_list[raw_message])
             return False
         return False
+    
+    def easy_reply_command(self,cmd):
+        if len(cmd)==2:
+            if cmd[0]=='set':
+                try:
+                    self.config['manager']=int(cmd[1])
+                    with open(os.path.join(self.dir,'config.json'),'w') as f:
+                        f.write(json.dumps(self.config))
+                    self.log.info('设置成功')
+                except:
+                    self.log.warning('参数错误')
+            else:
+                self.log.warning('参数错误')
+        else:
+            self.log.warning('参数错误')
 
 
 plugin_name="简单的自动回复"
